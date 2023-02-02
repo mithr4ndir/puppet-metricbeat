8b39c260ff1c486c6c837b534edb57d7e1bdbcda
Chris Ladino <ladino@uber.com>
2023-02-02 12:35:17 -0800

    adding unzip support for windows core

--- manifests/install.pp
+++ manifests/install.pp
@@ -17,19 +17,26 @@ class metricbeat::install inherits metricbeat {
     }
 
     if !defined(File[$metricbeat::install_dir]) {
-      file{$metricbeat::install_dir:
+      file { $metricbeat::install_dir:
         ensure => directory,
       }
     }
 
-    archive{ $zip_file:
+    archive { $zip_file:
       source       => $metricbeat::real_download_url,
       cleanup      => false,
       creates      => $version_file,
       proxy_server => $metricbeat::proxy_address,
     }
-    exec{"unzip ${filename}":
-      command => "\$sh=New-Object -COM Shell.Application;\$sh.namespace((Convert-Path '${metricbeat::install_dir}')).Copyhere(\$sh.namespace((Convert-Path '${zip_file}')).items(), 16)", # lint:ignore:140chars
+    if $facts['powershell_version'] =~ 5 {
+      $unzip_command = "Expand-Archive ${zip_file} \"${metricbeat::install_dir}\""
+    }
+    else {
+      $unzip_command = "\$sh=New-Object -COM Shell.Application;\$sh.namespace((Convert-Path '${metricbeat::install_dir}')).Copyhere(\$sh.namespace((Convert-Path '${zip_file}')).items(), 16)" # lint:ignore:140chars
+    }
+
+    exec { "unzip ${filename}":
+      command => $unzip_command,
       creates => $version_file,
       require => [
         File[$metricbeat::install_dir],
@@ -38,30 +45,30 @@ class metricbeat::install inherits metricbeat {
     }
 
     # Clean up after ourselves
-    file{$zip_file:
+    file { $zip_file:
       ensure  => absent,
       backup  => false,
       require => Exec["unzip ${filename}"],
     }
 
     # You can't remove the old dir while the service has files locked...
-    exec{"stop service ${filename}":
+    exec { "stop service ${filename}":
       command => 'Set-Service -Name metricbeat -Status Stopped',
       creates => $version_file,
       onlyif  => 'if(Get-WmiObject -Class Win32_Service -Filter "Name=\'metricbeat\'") {exit 0} else {exit 1}',
       require => Exec["unzip ${filename}"],
     }
-    exec{"rename ${filename}":
+    exec { "rename ${filename}":
       command => "Remove-Item '${install_folder}' -Recurse -Force -ErrorAction SilentlyContinue;Rename-Item '${metricbeat::install_dir}/${filename}' '${install_folder}'", # lint:ignore:140chars
       creates => $version_file,
       require => Exec["stop service ${filename}"],
     }
-    exec{"mark ${filename}":
+    exec { "mark ${filename}":
       command => "New-Item '${version_file}' -ItemType file",
       creates => $version_file,
       require => Exec["rename ${filename}"],
     }
-    exec{"install ${filename}":
+    exec { "install ${filename}":
       cwd         => $install_folder,
       command     => './install-service-metricbeat.ps1',
       refreshonly => true,
@@ -76,7 +83,7 @@ class metricbeat::install inherits metricbeat {
       $package_ensure = $metricbeat::ensure
     }
 
-    package{'metricbeat':
+    package { 'metricbeat':
       ensure => $package_ensure,
     }
   }
